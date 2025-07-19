e52497b6508f2f5e222df0cb290e7ce5d8645beb
jiehuang239 <jh4000@columbia.com>
2025-07-19 15:13:31 -0700

    add exercise lib

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/Workouts Assets/data/exercises.json';
 
 export default function App() {
+  const exercise = exercises[0];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.exerciseName}>{exercise.name}</Text>
+      <Text style={styles.exerciseEquipment}>Muscle: {exercise.muscle} | Equipment: {exercise.equipment}</Text>
+      <Text style={styles.exerciseInstructions}>Instructions: {exercise.instructions}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  exerciseName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  exerciseEquipment: {
+    color: 'dimgray',
+  },
+  exerciseInstructions: {
+    marginTop: 10,
   },
 });
